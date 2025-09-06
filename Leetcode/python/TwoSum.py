48a14a3459e22c7d07b95715371d1ca2cdfa0737
manojkumartechie <manojkumar9384@outlook.com>
2025-09-06 13:51:26 +0530

    TwoSum_completed

--- Leetcode/python/TwoSum.py
+++ Leetcode/python/TwoSum.py
@@ -1,12 +1,10 @@
 class Solution:
-    class TwoSumSolver:
-        def find_two_sum(self, nums: list[int], target: int) -> list[int]:
-            seen = {}
-            for i, num in enumerate(nums):
-                if target - num in seen:
-                    return [seen[target - num], i]
-                seen[num] = i
-
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        return Solution.TwoSumSolver().find_two_sum(nums, target)
-
+        lookup = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in lookup:
+                return [lookup[complement], i]
+            lookup[num] = i
+        
+        return [0, 0]  
